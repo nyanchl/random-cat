0a336b56c66ae50176f2d314d59a33b7a27d2e19
nyanchi <rotdicm@gmail.com>
2023-04-20 14:27:31 +0900

    猫の画像を取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
 
-const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+const fetchImage = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
 };
-export default IndexPage;
