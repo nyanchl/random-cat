701d77860c7e933460d7c70d1e2b9a0735099acd
nyanchi <rotdicm@gmail.com>
2023-04-22 11:33:51 +0900

    猫の画像をランダム生成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,57 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+ 
+const IndexPage: NextPage = () => {
+  // ❶ useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  // ❷ マウント時に画像を読み込む宣言
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url); // 画像URLの状態を更新する
+      setLoading(false); // ローディング状態を更新する
+    });
+  }, []);
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+};
+export default IndexPage;
 
-const fetchImage = async () => {
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
-  const images = await res.json();
-  console.log(images);
+  const images: unknown = await res.json();
+  if (!Array.isArray(images)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  const image: unknown = images[0];
+  // Imageの構造をなしているか？
+  if (!isImage(image)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
   return images[0];
 };
+
+const isImage = (value: unknown): value is Image => {
+  // 値がオブジェクトなのか？
+  if (!value || typeof value !== "object") {
+    return false;
+  }
+  // urlプロパティが存在し、かつ、それが文字列なのか？
+  return "url" in value && typeof value.url === "string";
+};
+
+fetchImage(); // 追加
